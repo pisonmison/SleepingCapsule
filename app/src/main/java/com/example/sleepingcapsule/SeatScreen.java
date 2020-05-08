a270de530f6ad64b1cdd3d10e05f98cafeb1596b
pisonmison <fattony2009@yandex.com>
2020-05-08 15:57:07 +0200

    Man kann jetzt durch Button click in MainActivity in die Acivity 2 aka LightScreen wechseln

--- app/src/main/java/com/example/sleepingcapsule/SeatScreen.java
+++ app/src/main/java/com/example/sleepingcapsule/SeatScreen.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SeatScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_seat_screen);
     }
 }
