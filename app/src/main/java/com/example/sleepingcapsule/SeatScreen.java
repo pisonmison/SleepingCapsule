7e20c0b77aea2ecd0b5a49ab5954e79a123dbfa7
pisonmison <58531355+pisonmison@users.noreply.github.com>
2020-05-24 17:05:33 +0200

    UPDATE 24.05. Layout Linien geeinigt

--- app/src/main/java/com/example/sleepingcapsule/SeatScreen.java
+++ app/src/main/java/com/example/sleepingcapsule/SeatScreen.java
@@ -1,10 +1,31 @@
 package com.example.sleepingcapsule;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class SeatScreen extends AppCompatActivity {
+
+
+   private ImageButton favPos;
+   private ImageButton lyingPos;
+   private ImageButton zeroGPos;
+   private ImageButton sittingPos;
+
+   private LinearLayout buttonlayout;
+   private LinearLayout sliderlayout;
+
+   private Button saveButton;
+
+   //button bar layout??
+    private
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
