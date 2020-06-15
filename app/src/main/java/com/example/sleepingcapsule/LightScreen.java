6afa84ac6430ebccb48b894170df5d51c2a5236a
muki <mukara92@web.de>
2020-06-15 12:57:29 +0200

    pick from colorwheel

--- app/src/main/java/com/example/sleepingcapsule/LightScreen.java
+++ app/src/main/java/com/example/sleepingcapsule/LightScreen.java
@@ -3,13 +3,17 @@ package com.example.sleepingcapsule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LightScreen extends AppCompatActivity implements Button.OnClickListener {
-
 
     private ImageView seatIcon;
     private ImageView lightIcon;
@@ -18,10 +22,48 @@ public class LightScreen extends AppCompatActivity implements Button.OnClickList
     private String currentScreen = "light";
 
 
+    ImageView mColorwheel;
+    TextView mHex;
+    View mAusgabe;
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_screen);
+
+        mColorwheel = findViewById(R.id.colorwheel);
+        mHex = findViewById(R.id.hex);
+        mAusgabe = findViewById(R.id.ausgabe);
+
+        mColorwheel.setDrawingCacheEnabled(true);
+        mColorwheel.buildDrawingCache(true);
+
+        mColorwheel.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
+                    bitmap = mColorwheel.getDrawingCache();
+
+                    int pixel = bitmap.getPixel((int)event.getX(), (int)event.getY());
+
+                    int r = Color.red(pixel);
+                    int g = Color.green(pixel);
+                    int b = Color.blue(pixel);
+
+                    String hex ="#"+ Integer.toHexString(pixel);
+
+                    mAusgabe.setBackgroundColor(Color.rgb(r,g,b));
+
+                    mHex.setText("RGB: "+ r +", "+  g +", "+ b +"\nHex: "+ hex);
+
+
+                }
+
+                return true;
+            }
+        });
+
+
 
         //taskbar iniation
         mContext = this;
@@ -39,6 +81,8 @@ public class LightScreen extends AppCompatActivity implements Button.OnClickList
 
 
     }
+
+
 
 
 
